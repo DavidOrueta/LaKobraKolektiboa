36b60a3bb69e1032f99b782a6ebe0cb6f5bbcbef
David <orueta.david@gmail.com>
2026-04-12 20:08:48 +0200

    db

--- BackendLaKobra/config/db.php
+++ BackendLaKobra/config/db.php
@@ -1,16 +1,4 @@
 <?php
-<<<<<<< HEAD
-$host = "localhost";
-$db   = "lakobra";
-$user = "root";
-$pass = "";
- 
-try {
-    $conn = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Error de conexión: " . $e->getMessage());
-=======
 function conectarDB() {
     $host = "localhost";
     $db = "lakobra";
@@ -24,13 +12,6 @@ function conectarDB() {
     } catch (PDOException $e) {
         die("Error de conexión: " . $e->getMessage());
     }
->>>>>>> 8fce333ebbb3df65a2a618a6740f3c0b19d0cdd2
-}
- 
 
-function conectarDB() {
-    global $conn;
-    return $conn;
 }
-?>
  
